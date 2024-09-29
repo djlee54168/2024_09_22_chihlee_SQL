287781940e3a8fa5161a221b834132fa190be49f
djlee54168 <djlee54168@gmail.com>
2024-09-29 16:30:29 +0800

    更新上傳

--- SQL/lesson2_2.sql
+++ SQL/lesson2_2.sql
@@ -1,11 +1,25 @@
 DROP TABLE IF EXISTS hospital;
 
 CREATE TABLE IF NOT EXISTS hospital (
-	A SERIAL PRIMARY KEY,
-	B VARCHAR(5) NOT NULL,
-	C VARCHAR(30) NOT NULL UNIQUE,
-	D VARCHAR(15),
-	E VARCHAR(7),
-	F VARCHAR(7),
-	G VARCHAR(50) 
+	id SERIAL PRIMARY KEY,
+	state VARCHAR(5) NOT NULL,
+	name VARCHAR(30) NOT NULL UNIQUE,
+	tel VARCHAR(15),
+	ext VARCHAR(7),
+	contact VARCHAR(7),
+	addr VARCHAR(50)
+	);
+
+/*城市,啟始時間,結束時間,最高溫度,最低溫度,感覺*/
+
+DROP TABLE IF EXISTS weather;
+
+CREATE TABLE IF NOT EXISTS weather(
+    id SERIAL PRIMARY KEY,
+	city VARCHAR(20) NOT NULL,
+    startDate timestamp,
+	endDate timestamp,
+	hight real,
+	low real,
+	status VARCHAR(20)
 );
