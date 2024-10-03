b2a2cff889a11c514181e09ad37f31c3b62c79d9
djlee54168 <djlee54168@gmail.com>
2024-10-03 12:18:48 +0800

    更改資料表名稱為stations

--- SQL/HOMEWORK2_1.sql
+++ SQL/HOMEWORK2_1.sql
@@ -1,12 +1,12 @@
 /*車站代號,車站中文名稱,車站英文名稱*/
 
-DROP TABLE IF EXISTS ticket;
+DROP TABLE IF EXISTS stations;
 
-CREATE TABLE IF NOT EXISTS ticket(
+CREATE TABLE IF NOT EXISTS stations(
 	id SERIAL PRIMARY KEY,
 	code VARCHAR(4) NOT NULL UNIQUE,
 	name VARCHAR(10) UNIQUE,
 	e_name VARCHAR(40)
 );
 
-SELECT * FROM ticket;
+SELECT * FROM stations;
