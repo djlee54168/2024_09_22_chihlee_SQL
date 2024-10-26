8912787d593ef7cf6019f5870956ec5ab40aa1fa
djlee54168 <djlee54168@gmail.com>
2024-10-26 11:33:17 +0800

    更新

--- SQL/lesson2_1.sql
+++ SQL/lesson2_1.sql
@@ -3,3 +3,5 @@ DATABASE
 mydatabase_a09;
 
 DROP DATABASE IF EXISTS mydatabase_a09;
+
+postgresql://djlee54168_user:22gJBpbdHc6riR7HEL3ZpPn0YgIJy8ib@dpg-crnssso8fa8c738kfee0-a.singapore-postgres.render.com/djlee54168
