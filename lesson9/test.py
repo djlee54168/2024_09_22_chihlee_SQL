d84e196a072e07cf9a8f72e9beaaf0f6eb6f73e4
djlee54168 <djlee54168@gmail.com>
2024-11-25 22:15:49 +0800

    更新

--- lesson9/test.py
+++ lesson9/test.py
@@ -4,12 +4,20 @@ import streamlit as st
 from time import sleep
 from procedure import get_data
 load_dotenv()
-placeholder = st.empty()
-with placeholder:
-    with st.spinner('下載資料中...'):
-        get_data()
 
-    st.success('下載完成',icon='✅') # 直接將圖示貼至''中
+@st.dialog('目前發生問題，請稍後再試')
+def alert():
+        st.write('連線有問題')
+        st.stop()
+
+with st.spinner('下載資料中...'):
+        get_data()
+alert()
+st.write('顯示頁面')
+
+
+
+
+    # st.success('下載完成',icon='✅') # 直接將圖示貼至''中
     # st.success('下載完成',icon=':material/delete:') # 來源：https://fonts.google.com/icons
-    sleep(2)
-placeholder.empty()
+
